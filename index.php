5d187d29d62d93c9f175589c71595546982acaac
EnzaEnzi <enza.webdev@gmail.com>
2021-12-23 21:39:21 +0100

    :bug: add index

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Name file: 404
+ * Description:
+ *
+ * @package WordPress
+ * @subpackage MyCV
+ * @since 1.0.0
+ */
+?>
+
+
+<?php get_header() ?>
+    <main>
+        <h1>this is main page</h1>
+    </main>
+<?php get_footer() ?>
