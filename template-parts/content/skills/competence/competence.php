e62f03e2084e7fa49c8ead734728a68ec61bd4f4
EnzaEnzi <enza.webdev@gmail.com>
2022-06-20 18:33:15 +0200

    :zap: [front] recovery information of DB

--- template-parts/content/skills/competence/competence.php
+++ template-parts/content/skills/competence/competence.php
@@ -10,17 +10,41 @@
  */
 ?>
 
-<!--  if loop -> ok  [Post-type  => 'experiences']  -->
+<?php
+    wp_reset_postdata();
 
-<!--  else  -->
-<div class="no-result">
-    <!--  PROVISOIR  -->
-    <?php if(get_locale() === 'fr_FR') : // Partie FR =============== ?>
-        <?php echo get_option('skill_else_msg_fr'); ?>
-    <?php elseif(get_locale() === 'en_GB') : // Partie EN =========== ?>
-        <?php echo get_option('skill_else_msg_en'); ?>
-    <?php elseif(get_locale() === 'it_IT') : // Partie EN =========== ?>
-        <?php echo get_option('skill_else_msg_it'); ?>
-    <?php endif; ?>
-</div>
-<!--  endif  -->
+    $args = array(
+        'post_type'      => 'competences',
+        'posts_per_page' => -1,
+        'orderby'        => 'id',
+        'order'          => 'DESC'
+    );
+    $my_query = new WP_query($args);
+    if($my_query->have_posts()) : while($my_query->have_posts()) : $my_query->the_post();
+?>
+    <div>
+        <?php the_title(); ?>
+
+        <?php
+            $list_skill = get_post_meta($post->ID, 'list_skill', true);
+            foreach ($list_skill as $field) :
+        ?>
+            <?php echo $field['name']; ?>
+            <?php echo $field['percent']; ?>
+        <?php endforeach; ?>
+    </div>
+
+<?php endwhile; else: ?>
+    <div class="no-result">
+
+        <p class="display-1">&#128549;</p>
+
+        <?php if(get_locale() === 'fr_FR') : // Partie FR =============== ?>
+            <p class="no-result-msg"><?php echo get_option('skill_else_msg_fr'); ?></p>
+        <?php elseif(get_locale() === 'en_GB') : // Partie EN =========== ?>
+            <p class="no-result-msg"><?php echo get_option('skill_else_msg_en'); ?></p>
+        <?php elseif(get_locale() === 'it_IT') : // Partie EN =========== ?>
+            <p class="no-result-msg"><?php echo get_option('skill_else_msg_it'); ?></p>
+        <?php endif; ?>
+    </div>
+<?php endif;  wp_reset_postdata(); ?>
